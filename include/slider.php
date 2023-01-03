5eb1fd5fc2d8761fbd2bcea88512860ece4e4318
durjoydey12 <durjoydey663@gmail.com>
2023-01-03 13:38:05 +0600

    all

--- include/slider.php
+++ include/slider.php
@@ -1,18 +1,19 @@
 <?php include 'include/connection.php';
-$sql = "SELECT * FROM sliders ";
+$sql = "SELECT * FROM sliders";
 $result = $db->query($sql);
-?>
+  ?>
 <slider>
-
+<?php while($row = $result->fetch_assoc()): ?>
+      
             <div id="carouselExampleFade" class="carousel slide carousel-fade">
               <div class="carousel-inner">
                       <div class="carousel-item active">
-                      <img src="images/image1.jpeg" height="450px" width="1500px"  alt="img">
+                      <img src="<?php echo "admin/images/" .$row['image'] ; ?>" height="450px" width="1500px"  alt="img">
                       </div>
               </div>   
                       
             </div>
-
+<?php endwhile; ?>
             
 </slider>
 
