02dba1af68d64959869a840bb844c8cf7c468231
durjoydey12 <durjoydey663@gmail.com>
2023-01-06 20:45:25 +0600

    all

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                     <h2>Our Services</h2>
                     <hr><br>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
-            </div><br>
+            </div>br>
 
             <div class="row">
                 <div class="col-xs-10 col-md-6">
@@ -55,7 +55,7 @@
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.</p>
                                     </div>
                                </div>
-                     </div>
+             </div>
            
         </div>
 </div>
@@ -63,102 +63,86 @@
 
 
 <!-- Gallery Section -->
-<!-- <div id="portfolio">
+<div id="portfolio">
   <div class="container">
     <div class="section-title text-center center">
       <h2>Project Gallery</h2>
       <hr>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
     </div>
- 
     <div class="row">
-      <div class="portfolio-items">
-        <div class="col-sm-6 col-md-4 lawn">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Lorem Ipsum</h4>
-              </div>
-              <img src="img/portfolio/01-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 planting">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/02-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Adipiscing Elit</h4>
-              </div>
-              <img src="img/portfolio/02-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 lawn">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Lorem Ipsum</h4>
-              </div>
-              <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 lawn">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Lorem Ipsum</h4>
-              </div>
-              <img src="img/portfolio/04-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 planting">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Adipiscing Elit</h4>
-              </div>
-              <img src="img/portfolio/05-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 garden">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Dolor Sit</h4>
-              </div>
-              <img src="img/portfolio/06-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 garden">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Dolor Sit</h4>
-              </div>
-              <img src="img/portfolio/07-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 lawn">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/08-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Lorem Ipsum</h4>
-              </div>
-              <img src="img/portfolio/08-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 planting">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-              <div class="hover-text">
-                <h4>Adipiscing Elit</h4>
-              </div>
-              <img src="img/portfolio/09-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                <div class="col-xs-12 col-md-4">
+                            <div class="service-media"> 
+                                <img src="images/image.jpg" alt="img" height="250px" width="350px"> 
+                            </div>
+                                    
+                </div>
+            
+
+                                <div class="col-xs-12 col-md-4">
+                                    <div class="service-media"> <img src="images/19.jpg" alt=" img"height="250px" width="350px" > </div>
+                                        
+                               </div>
+
+                               
+                               <div class="col-xs-12 col-md-4">
+                                    <div class="service-media"> <img src="images/19.jpg" alt=" img"height="250px" width="350px"> </div>
+                                        
+                               </div>                         
+             </div>
+          <br><br>
+
+
+             <div class="row">
+                <div class="col-xs-12 col-md-4">
+                            <div class="service-media"> 
+                                <img src="images/image.jpg" alt="img" height="250px" width="350px"> 
+                            </div>
+                                    
+                </div>
+            
+
+                                <div class="col-xs-12 col-md-4">
+                                    <div class="service-media"> <img src="images/19.jpg" alt=" img"height="250px" width="350px" > </div>
+                                        
+                               </div>
+
+                               
+                               <div class="col-xs-12 col-md-4">
+                                    <div class="service-media"> <img src="images/image80.jpg" alt=" img"height="250px" width="350px"> </div>
+                                        
+                               </div>                         
+             </div>
+
+             <br><br>
+
+
+                    <div class="row">
+                    <div class="col-xs-12 col-md-4">
+                                <div class="service-media"> 
+                                    <img src="images/image.jpg" alt="img" height="250px" width="350px"> 
+                                </div>
+                                        
+                    </div>
+
+
+                                    <div class="col-xs-12 col-md-4">
+                                        <div class="service-media"> <img src="images/19.jpg" alt=" img"height="250px" width="350px" > </div>
+                                            
+                                    </div>
+
+                                    
+                                    <div class="col-xs-12 col-md-4">
+                                        <div class="service-media"> <img src="images/19.jpg" alt=" img"height="250px" width="350px"> </div>
+                                            
+                                    </div>                         
+                    </div>
+
+           
+
+    
   </div>
-</div> --> 
-       
+</div>
        
 
 
