d386d179496c9babbbd7e672cfd6a444a6fbc4ef
durjoydey12 <durjoydey663@gmail.com>
2023-01-06 13:17:14 +0600

    all

--- index.php
+++ index.php
@@ -2,7 +2,166 @@
 <?php include 'include/header.php' ; ?>
 
     <!-- slider -->
-    <?php include 'include/slider.php' ; ?>
+    <?php include 'include/slider.php' ; ?><br><br>
+    
+        <div id="about">
+            <div class="container">
+                <div class="row">
+                        <div class="col-xs-12 col-md-8">
+
+                                <div class="about-text">
+                                    <h2 class="text-info"> <span class="text-dark">Welcome to </span> Pritilata Cultural <br>Organization</h2>
+                                    <hr>
+                                    <b> </b>
+                                
+                                </div>
+                        </div>
+                            
+                        <div class="col-xs-12 col-md-3">
+                            <div class="about-media"> <img src="images/image69.jpg" alt="img " height="350px" width="500px" > </div>
+                            
+                        </div>
+                </div>
+            </div>
+        </div><br><br><br>
+        
+
+        <!-- Services Section -->
+        
+<div id="services">
+        <div class="container">
+            <div class="col-md-10 col-md-offset-1 section-title text-center">
+                    <h2>Our Services</h2>
+                    <hr><br>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+            </div><br>
+
+            <div class="row">
+                <div class="col-xs-10 col-md-6">
+                            <div class="service-media"> 
+                                <img src="images/image.jpg" alt="img" height="300px" width="500px" > 
+                            </div>
+                                    <div class="service-desc">
+                                        <h3>Outsourcisng</h3>
+                                        <p><b>According to the Outsourcing definition, it is a technique of appointing another firm or company for a specific task. To simplify, we can say that it is the business practice to set specific third-party to give services previously done by the   company’s staff. The foremost cause of doing so is to cut down the costs and increase the company’s profitability. </b></p>
+                                    </div>
+                </div>
+            
+
+                                <div class="col-xs-10 col-md-6">
+                                    <div class="service-media"> <img src="images/19.jpg" alt=" img" height="300px" width="500px" > </div>
+                                        <div class="service-desc">
+                                            <h3>Institute</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.</p>
+                                    </div>
+                               </div>
+                     </div>
+           
+        </div>
+</div>
+
+
+
+<!-- Gallery Section -->
+<!-- <div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Project Gallery</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+    </div>
+ 
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-4 lawn">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Lorem Ipsum</h4>
+              </div>
+              <img src="img/portfolio/01-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 planting">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/02-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Adipiscing Elit</h4>
+              </div>
+              <img src="img/portfolio/02-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 lawn">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Lorem Ipsum</h4>
+              </div>
+              <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 lawn">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Lorem Ipsum</h4>
+              </div>
+              <img src="img/portfolio/04-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 planting">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Adipiscing Elit</h4>
+              </div>
+              <img src="img/portfolio/05-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 garden">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Dolor Sit</h4>
+              </div>
+              <img src="img/portfolio/06-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 garden">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Dolor Sit</h4>
+              </div>
+              <img src="img/portfolio/07-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 lawn">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/08-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Lorem Ipsum</h4>
+              </div>
+              <img src="img/portfolio/08-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 planting">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Adipiscing Elit</h4>
+              </div>
+              <img src="img/portfolio/09-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> --> 
+       
+       
+
+
 
 <!-- footer -->
  <?php include 'include/footer.php' ; ?>
