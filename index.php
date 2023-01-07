5aa8359cd5c6b2744d544770ad2fb17e98adb573
dabidey93 <dabidey93@gmail.com>
2023-01-07 10:39:03 +0600

    all

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
                                 <div class="about-text">
                                     <h2 class="text-info"> <span class="text-dark">Welcome to </span> Pritilata Cultural <br>Organization</h2>
                                     <hr>
-                                    <b>......................................</b>
+                                    <b>.To pursue higher education, Pritilata went to Calcutta (now Kolkata) and got admitted to
+                                         the Bethune College. Two years later, she graduated in philosophy from the college with a distinction.[20] However, her degree was withheld by the Calcutta University administration.
+                                         In 2012, she (and Bina Das) were conferred their certificates of merit posthumously.....</b>
                                 
                                 </div>
                         </div>
@@ -33,7 +35,9 @@
             <div class="section-title text-center center">
                     <h2>Our Services</h2>
                     <hr><br>
-                    <p>.............................................</p>
+                    <p>A service is something that the public needs, such as transport, communications 
+                        facilities, hospitals, or energy supplies, which is provided in a
+                         planned and organized way by the government or an official body...</p>
             </div><br>
 
             <div class="row">
@@ -43,7 +47,13 @@
                             </div>
                                     <div class="service-desc">
                                         <h3>Outsourcisng</h3>
-                                        <p><b>........................................................................ </b></p>
+                                        <p><b>Outsourcing has become a normal practice in business. Very often, 
+                                            we don’t even see it as a separate category. There are some criteria that
+                                             help divide outsourcing companies into certain groups. The first one is the
+                                              specifics of their activity, the next one is the number and specificity of
+                                               the transferred functions, and the last criterion is geopolitical data, which is
+                                                clearly visible in IT-outsourcing. Employees can work from different countries
+                                                 all over the world,creating a single product... </b></p>
                                     </div>
                 </div>
             
@@ -52,7 +62,9 @@
                                     <div class="service-media"> <img src="images/19.jpg" alt=" img" height="300px" width="500px" > </div>
                                         <div class="service-desc">
                                             <h3>Institute</h3>
-                                            <p>.......................................................................</p>
+                                            <p>educational institution is a place where people of different ages gain an education,
+                                             including preschools, childcare, primary-elementary schools, secondary-high schools, and universities.
+                                             They provide a large variety of learning environments and learning spaces.[....</p>
                                     </div>
                                </div>
              </div>
@@ -68,7 +80,9 @@
     <div class="section-title text-center center">
       <h2>Our Gallery</h2>
       <hr>
-      <p>..........................................................................</p>
+      <p>The gallery’s wall configuration, which has been fixed for quite some time,
+         has now been opened up in refreshingly spacious ways that allow a viewer to visually
+          cross-reference paintings......</p>
     </div>
     <div class="row">
                 <div class="col-xs-12 col-md-4">
