65dffb11009812da908b5948908cd4f434281852
durjoydey12 <durjoydey663@gmail.com>
2023-01-09 00:06:47 -0800

    all

--- admin/outsourcisng3_users.php
+++ admin/outsourcisng3_users.php
@@ -47,7 +47,7 @@ $result = $db->query($sql);
                             <td><?php echo $row['title'] ; ?></td>
                             
                             
-                            <td><?php echo $row['image'] ; ?></td>
+                            <td><img src="<?php echo "images/".$row['image'] ; ?>"width="50" alt=""></td>
                         
                         <td>
                             <a href="edit_outsourcisng3.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a>
